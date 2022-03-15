d05e7ad9a93e186f105c4401f0fca3cc972a54a4
rhj3erhuk3rh3 <ruslanaskar361@gmail.com>
2022-03-15 20:48:14 +0600

    querty

---
+++ Lesson3/main.js
@@ -0,0 +1,63 @@
+ const a ="i";
+
+ for(let key of[]){
+
+ }
+  while (1<0) {
+
+
+// if (a===b) {
+//  function syaHello() {
+ //  console.log("Hello")
+//  }
+//  syaHello("Hello")
+ }
+ function getFullName(user,age) {
+     if  (age<18) {
+   console.log("Close")
+  } else {
+   console.log("Open")
+  }
+ }
+ const user = {
+  first_name: "Jonh",
+  last_name: "Kia"
+
+ }
+ const user2 = {
+  first_name: "Aktan",
+  last_name: "batman"
+ }
+ const user3 = {
+   first_name:"Atabek",
+   last_name:"super_rovno",
+ }
+  getFullName(user, 20)
+  getFullName(user2,16)
+  getFullName(user3,15)
+ //
+// console.log(user.first_name,user.last_name);
+// console.log(user2.first_name,user.last_name);
+// console.log(user3.first_name,user.last_name);
+let werty=[1,2,3,4,5,6,7,8]
+ let qwerty=[1,2,3,4,5,6]
+function twomass(){
+  if (werty.length < qwerty.length){
+   alert(`первый меньше`)
+  }else if (werty.length > qwerty.length){
+   alert(`первый больше`)
+  }else{
+   console.log(`первый равен`)
+  }
+}
+ twomass()
+ let ero=`fgefgdgfedgfdhfg`
+ function countchar(){
+  console.log(ero.length)
+ }
+ countchar()
+
+
+
+
+
